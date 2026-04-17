fa757f1f696251df084e25bdec7bc30debf2ac93
Cristiano Carvalho <cristiano.carvalho23@icloud.com>
2026-04-17 15:09:29 -0300

    fix(treesitter): restore gleam highlighting

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,26 +1,40 @@
+local filetypes = { "elixir", "heex", "eex", "gleam", "lua", "vim", "vimdoc", "markdown" }
+local indent_filetypes = {
+  elixir = true,
+  heex = true,
+  eex = true,
+  gleam = true,
+  lua = true,
+  markdown = true,
+}
+
 return {
-  -- Tree-sitter for better syntax highlighting
+  -- Tree-sitter for syntax highlighting and indentation
   {
     "nvim-treesitter/nvim-treesitter",
+    lazy = false,
     build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile" },
+    cmd = { "TSInstall", "TSUpdate", "TSUninstall", "TSLog" },
     config = function()
-      local status_ok, configs = pcall(require, 'nvim-treesitter.configs')
+      local status_ok, treesitter = pcall(require, "nvim-treesitter")
       if not status_ok then
         return
       end
 
-      configs.setup({
-        ensure_installed = { "elixir", "heex", "eex", "lua", "vim", "vimdoc", "markdown" },
-        sync_install = false,
-        auto_install = true,
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = false,
-        },
-        indent = {
-          enable = true,
-        },
+      treesitter.setup()
+      pcall(treesitter.install, filetypes)
+
+      local group = vim.api.nvim_create_augroup("UserTreesitter", { clear = true })
+      vim.api.nvim_create_autocmd("FileType", {
+        group = group,
+        pattern = filetypes,
+        callback = function(args)
+          pcall(vim.treesitter.start, args.buf)
+
+          if indent_filetypes[vim.bo[args.buf].filetype] then
+            vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+          end
+        end,
       })
     end,
   },
